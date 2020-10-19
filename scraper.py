9cb724646205d4c545b3b411f0b2931f522f633e
james@jwilkerson.me <james@jwilkerson.me>
2020-10-19 14:30:24 -0500

    Turned off slack notifications

--- scraper.py
+++ scraper.py
@@ -20,11 +20,11 @@ DSHS_REPAIRED_FILE = os.environ.get('REPAIR_FILE')
 
 
 def calculate_averages(daily_values):
-  """ 
+  """
   Takes a list of daily new cases for a county
   and calculates a 7-day rolling average for the past
-  14 days 
-  """ 
+  14 days
+  """
 
   averages = []
   for i in range(7, len(daily_values)):
@@ -35,7 +35,7 @@ def calculate_averages(daily_values):
 
     try:
       first_day = daily_values[i - 7].replace(',', '')
-    except AttributeError: 
+    except AttributeError:
       first_day = daily_values[i - 7]
 
     avg = round((int(last_day) - int(first_day)) / 7, 2)
@@ -43,14 +43,14 @@ def calculate_averages(daily_values):
 
   return(averages)
 
-def repair_file(): 
-  """ 
+def repair_file():
+  """
   In an instance where daily totals are not collected
-  (scraper failure, state data format change, etc), and 
+  (scraper failure, state data format change, etc), and
   daily totals need to be manually backfilled, this function
   will take the json file containing the correct daily numbers
   for the county and recalculate 7-day averages
-  """ 
+  """
 
   repair_request = requests.get(DSHS_REPAIRED_FILE)
   repair_data = repair_request.json()
@@ -61,7 +61,7 @@ def repair_file():
   return(repair_data)
 
 def update_trends():
-  """ 
+  """
   Function run daily to check for updated data from DSHS.
   If the DSHS data has updated since our last pull, grab the
   day's positive count for each county, and append it
@@ -80,7 +80,7 @@ def update_trends():
   # dshs data file that contains last county update date
   update_request = requests.get(DSHS_UPDATE_FILE)
   update_data = update_request.json()
-  
+
 
  # pulling last dshs update date
   last_update = datetime.date.fromtimestamp(update_data['editingInfo']['lastEditDate']/1000) - datetime.timedelta(days=1)
@@ -93,15 +93,15 @@ def update_trends():
   if (dshs_date != trend_data[0]['update_date']):
     for county in trend_data:
       county['update_date'] = dshs_date
-      
-      try: 
+
+      try:
         dshs_county_data = [item for item in dshs_data['features']
                             if item['attributes']['County']
                             == county['name']][0]['attributes']['Positive']
       except IndexError:
         # sometimes dshs spells DeWitt county with a space. check for that
         if county['name'] == 'DeWitt':
-            try: 
+            try:
               dshs_county_data = [item for item in dshs_data['features']
                                   if item['attributes']['County']
                                   == 'De Witt'][0]['attributes']['Positive']
@@ -109,7 +109,7 @@ def update_trends():
               dshs_county_data = 0
         else:
           dshs_county_data = 0
-      
+
       # ditch the first entry in our daily trend totals
       county['trend'] = county['trend'][1:]
 
@@ -120,23 +120,21 @@ def update_trends():
       county['averages'] = calculate_averages(county['trend'])
       slackMsg = 'DSHS county trend scraper updated with data from {0}.'.format(dshs_date)
 
-  else: 
+  else:
     slackMsg = 'DSHS county trend scraper ran, but no new data found. Last update occured {0}.'.format(trend_data[0]['update_date'])
     print('No update yet')
 
 
   slack = Slacker(os.environ.get('SLACK_TOKEN'))
-  slack.chat.post_message(
-        '#feed-coronavirus-scrapers',
-        slackMsg,
-        as_user=False,
-        icon_emoji='cardboardbox',
-        username='DSHS County Trend Scraper'
-        )
+  # slack.chat.post_message(
+  #       '#feed-coronavirus-scrapers',
+  #       slackMsg,
+  #       as_user=False,
+  #       icon_emoji='cardboardbox',
+  #       username='DSHS County Trend Scraper'
+  #       )
 
   return(trend_data)
 
 if __name__ == '__main__':
     globals()[sys.argv[1]]()
-
-
